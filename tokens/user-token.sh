c87c842261d4014094c9d57c958937c8f7e34258
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-10-03 11:42:44 +0100

    tokens: fix start time

--- tokens/user-token.sh
+++ tokens/user-token.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # user-token.sh helps creates booking tokens
-# example usage: user-token.sh https://book.practable.io "truss everyone" 86400
+# example usage: user-token.sh "https://book.practable.io" "truss everyone" 2122-10-12T07:20:50Z 86400
 export BOOKTOKEN_SECRET=$(cat ~/secret/book.pat)
 export BOOKTOKEN_AUDIENCE=$1
 export BOOKTOKEN_GROUPS=$2
