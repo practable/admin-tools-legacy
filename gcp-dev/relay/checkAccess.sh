ce25933a421a4c93a7cfd6f7cc0249e491ba61ab
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-01-27 18:17:05 +0000

    gcp-dev/relay (fix): improve report from checkAccess

--- gcp-dev/relay/checkAccess.sh
+++ gcp-dev/relay/checkAccess.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+# https://gist.github.com/angelo-v/e0208a18d455e2e6ea3c40ad637aac53
+# pad base64URL encoded to base64
+paddit() {
+  input=$1
+  l=`echo -n $input | wc -c`
+  while [ `expr $l % 4` -ne 0 ]
+  do
+    input="${input}="
+    l=`echo -n $input | wc -c`
+  done
+  echo $input
+}
+
+decodejwt() {
+# read and split the token and do some base64URL translation
+read h p s <<< $(echo $1 | tr [-_] [+/] | sed 's/\./ /g')
+
+h=`paddit $h`
+p=`paddit $p`
+
+# assuming we have jq installed
+echo $h | base64 -d | jq
+echo $p | base64 -d | jq
+
+exp=$(echo $p | base64 -d | jq '.exp')
+expdate=$(date -d "@${exp}")
+
+iat=$(echo $p | base64 -d | jq '.iat')
+iatdate=$(date -d "@${iat}")
+
+nbf=$(echo $p | base64 -d | jq '.nbf')
+nbfdate=$(date -d "@${nbf}")
+
+echo "exp: ${expdate}"
+echo "iat: ${iatdate}"
+echo "nbf: ${nbfdate}"
+}
+
+
 # Make token
 export RELAY_TOKEN_LIFETIME=30
 export RELAY_TOKEN_SCOPE_READ=true
@@ -8,15 +47,19 @@ export RELAY_TOKEN_SECRET=$(cat ~/secret/v0/relay.pat)
 export RELAY_TOKEN_TOPIC=stats
 export RELAY_TOKEN_AUDIENCE=https://dev.practable.io/access
 export client_token=$(relay token)
-echo "client_token=${client_token}"
+echo "client token:"
+echo ${client_token}
+echo ""
+echo "decoded:"
+decodejwt $client_token
 
 # Request Access
 export ACCESS_URL="${RELAY_TOKEN_AUDIENCE}/session/stats"
-echo $ACCESS_URL
-
-export RESP=$(curl -X POST \
+echo ""
+echo "access to ${ACCESS_URL}:"
+echo ""
+export RESP=$(curl -s -X POST \
 -H "Authorization: ${client_token}" \
 				   $ACCESS_URL)
-
 echo $RESP
-
+echo ""
