32245341f9e8550a8ee9b2fc6da263b5466cbab0
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-02-23 19:03:13 +0000

    aws/book (feat): add book-token.sh

---
+++ aws/book/book-token.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+#example book-token.sh "recruit everyone"
+
+if [ "$BOOK_SECRET" = "" ];
+then
+	echo 'you must set BOOK_SECRET'
+fi
+
+export BOOKTOKEN_SECRET=$(cat ~/secret/book.pat)
+export BOOKTOKEN_AUDIENCE=https://book.practable.io
+export BOOKTOKEN_LIFETIME=7776000
+export BOOKTOKEN_GROUPS=$1
+export BOOKTOKEN_ADMIN=true
+export BOOKUPLOAD_TOKEN=$(../bin/book token)
+echo "Admin token:"
+echo ${BOOKUPLOAD_TOKEN}
+echo ${BOOKUPLOAD_TOKEN}| decode-jwt
+
+# generate user token
+export BOOKTOKEN_ADMIN=false
+export USERTOKEN=$(book token)
+export BOOKJS_USERTOKEN=$(../bin/book token)
+echo "User token:"
+echo ${BOOKJS_USERTOKEN}
+echo ${BOOKJS_USERTOKEN} | decode-jwt
